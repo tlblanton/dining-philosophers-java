80b05a71d80788be9f0e3b60359e65f99a2135ee
tlblanton <tylerblanton635@gmail.com>
2016-03-06 09:27:42 -0700

    working, with display more or less correct (need to get rid of some extra printing stuff), and need to check random numbers

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -1,66 +1,39 @@
-package co.tylerblanton;
+//package co.tylerblanton;
+import java.util.ArrayList;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Lock;
 
 //use java util lock and unlock to avoid deadlocking
 
-public class DiningPhilosophers implements Runnable
+public class DiningPhilosophers
 {
     public static int numOfPhilosophers;
-    static final Philosopher[] philArr = new Philosopher[numOfPhilosophers];
+    public static ReentrantLock disp = new ReentrantLock();
+    public static ArrayList<Philosopher> philArr = new ArrayList<Philosopher>();
+    public static ArrayList<ReentrantLock> chopsticks = new ArrayList<>();
     public static void main(String[] args)
     {
         if(args.length != 0)
         {
             numOfPhilosophers = Integer.parseInt(args[0]);
+            for(int i = 0; i < numOfPhilosophers; ++i)
+            {
+                chopsticks.add(new ReentrantLock());        //there should now be 5 locks for the 5 chopsticks (given that args[0] is 5)
+            }
         }
         else
         {
             System.out.println("No command line arguments. Program terminated.");
             System.exit(1);
         }
-        //this makes it so there are 5 chopsticks for 5 philosophers
-        //each chopstick is a lock so that it can be locked or unlocked based on whether the philosopher has it or not
-        Lock[] chopsticks = new ReentrantLock[numOfPhilosophers];
-
-        //array of 'numOfPhilosophers' Philosophers
 
 
         for(int i = 0; i < numOfPhilosophers; ++i)
         {
-            philArr[i] = new Philosopher(i);
-            Thread t = new Thread(philArr[i]);
+            philArr.add(new Philosopher(i));
+            Thread t = new Thread(philArr.get(i));
             t.start();
         }
 
-
-            //(new Thread(new philArr[i]())).start();
-//        Philosopher p = new Philosopher();
-//        Philosopher p1 = new Philosopher();
-//        Philosopher p2 = new Philosopher();
-//        Thread t1 = new Thread(p1);
-//        Thread t2 = new Thread(p2);
-//        Thread t = new Thread(p);
-//        t.start();
-//        t1.start();
-//        t2.start();
-            //philArr[i] = new Philosopher(i, ).start()//not sure how to properly pass locks to each philosopher I'll be creating.
-            //create philosopher and call new phil.start()
-
-    }
-
-    @Override
-    public void run()
-    {
-        System.out.println("In here");
-    }
-
-    public static void display()
-    {
-        System.out.println("Philosopher\tState\tTimesEaten");
-        for(int i = 0; i < numOfPhilosophers; ++i)
-        {
-            System.out.println(philArr[i].number + "\t" + philArr[i].state + "\t" + philArr[i].timesEaten);
-        }
     }
 }
--- Philosopher.java
+++ Philosopher.java
@@ -1,7 +1,7 @@
-package co.tylerblanton;
+//package co.tylerblanton;
 import java.util.concurrent.locks.Lock;
 import java.util.Random;
-import co.tylerblanton.DiningPhilosophers;
+//import co.tylerblanton.DiningPhilosophers;
 
 /**
  * Created by tlblanton on 3/3/16.
@@ -9,10 +9,14 @@ import co.tylerblanton.DiningPhilosophers;
 
 //philosophers eat for less than 5 seconds, and
 
-public class Philosopher extends Thread
+public class Philosopher implements Runnable
 {
     boolean leftChopstick;
     boolean rightChopstick;
+    int leftNum;
+    int rightNum;
+    long eatTime;
+    long thinkTime;
     int timesEaten;
     String state;
     int number;
@@ -25,56 +29,204 @@ public class Philosopher extends Thread
     }
     public Philosopher(int newNum)
     {
+        leftChopstick = false;
+        rightChopstick = false;
+        eatTime = 5000;
+        leftNum = -1;
+        rightNum = -1;
         timesEaten = 0;
         number = newNum;
+        state = "hungry";
     }
 
 
     //each philosopher will have their own run through of this
     public void run()
     {
-        for(int i = 0; i < 10; i++)
+        try
         {
-            System.out.print(i);
+            while (timesEaten < DiningPhilosophers.numOfPhilosophers)
+            {
+                getLeftChopstick();     //try to get left. pick up if available and lock it.
+                getRightChopstick();    //if you have left, try to get right. If you can get right, lock it. If not, unlock left and wait
+                eat();  //If you have both chopsticks then increment timesEaten and wait(time spent eating), and then drop chopsticks.
+                //think();       //take some time to think after eating.. Think for T milliseconds. Eat for E. T<E
+            }
+            state = "Sleeping";
+            releaseChopsticks();
+            display();
+        }catch (Exception e)
+        {
+            System.out.println("leftnum is " + leftNum + " rightnum is  " + rightNum);
+            System.out.println("Philosopher " + number + " has stopped " + e);
         }
+    }
 
-        while(timesEaten < 5)
+    public void think()
+    {
+        state = "Thinking";
+        display();
+
+        Random randomGen  = new Random();
+        thinkTime = randomGen.nextInt((int)eatTime/1000);
+        thinkTime *= 1000;
+        long temp = thinkTime;
+        System.out.println("temp is " + temp + " eat time is " + eatTime);
+        try
         {
-//            getLeftChopstick();
-//            getRightChopstick();
-//            eat();  //wait in here
+            System.out.println("about to wait");
+            Thread.sleep(temp);
+        }catch(Exception e)
+        {
+            System.out.println("think sleep didn't go well");
         }
-        //while haven't eaten five times.
-        //left
-        //right
-        //eat
-        //wait
     }
 
     public void getLeftChopstick()
     {
-
-        //somehow grab left chopstick from collective pile.
+        state = "hungry";
+        display();
+        leftNum = grabChopstick();
+        if(leftNum != -1)
+        {
+            leftChopstick = true;
+        }
+        else
+        {
+            releaseChopsticks();
+        }
     }
+
     public void getRightChopstick()
     {
-        //try to grab another chopstick from collective pile
+        if(leftChopstick)
+        {
+            rightNum = grabChopstick();
+            if(rightNum != -1)
+            {
+                rightChopstick = true;
+            }
+            else
+            {
+                releaseChopsticks();
+            }
+        }
+        else
+        {
+            leftChopstick = false;
+            rightChopstick = false;
+            rightNum = -1;
+            leftNum = -1;
+        }
     }
+
+    int grabChopstick() //return -1 if no chopstick to grab, otherwise returns spot of lock that it "grabbed"
+    {
+        for(int i = 0; i < DiningPhilosophers.numOfPhilosophers; ++i)
+        {
+            if (DiningPhilosophers.chopsticks.get(i).isLocked() == false)
+            {
+                DiningPhilosophers.chopsticks.get(i).lock();
+                System.out.println("locking chopstick " + i);
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
     public void eat()
     {
+        Random randomGen = new Random();
+        if(leftChopstick && rightChopstick)
+        {
+            state = "Eating";
+            display();
 
+
+            try
+            {
+                Random rand = new Random();
+                eatTime = 2 + randomGen.nextInt(4);
+                eatTime *= 1000;
+                long temp = eatTime;
+
+                Thread.sleep(eatTime);
+            }catch(Exception e)
+            {
+                System.out.println("eat sleep didn't go well");
+            }
+            System.out.println("about to release. " + leftNum + " " + rightNum);
+
+            for(int i = 0; i < DiningPhilosophers.chopsticks.size(); ++i)
+            {
+                System.out.print(DiningPhilosophers.chopsticks.get(i).isLocked());
+            }
+
+            DiningPhilosophers.chopsticks.get(leftNum).unlock();    //releasing chopsticks and setting chopstick numbers to -1
+            DiningPhilosophers.chopsticks.get(rightNum).unlock();
+            leftNum = -1;
+            rightNum = -1;
+            leftChopstick = false;
+            rightChopstick = false;
+            timesEaten++;
+            think();
+
+        }
+        else
+        {
+            try
+            {
+                state = "hungry";
+                display();
+                releaseChopsticks();
+                System.out.println(number + " did not eat. Waiting for 2 seconds.");
+                Thread.sleep(2000);
+            }catch(Exception e)
+            {
+
+            }
+        }
     }
 
-    int grabChopstick() //return -1 if no chopstick to grab, 0 otherwise
+    void releaseChopsticks()
     {
-
-
-        return 0;
+        if(leftNum != -1)
+        {
+            DiningPhilosophers.chopsticks.get(leftNum).unlock();
+        }
+        leftChopstick = false;
+        leftNum = -1;
+        if(rightNum != -1)
+        {
+            DiningPhilosophers.chopsticks.get(rightNum).unlock();
+        }
+        rightChopstick = false;
+        rightNum = -1;
     }
 
-    void releaseChopstick(Lock l)
+    public void display()
     {
-        l.unlock();
+        while(DiningPhilosophers.disp.isLocked())
+        {
+            try
+            {
+                Thread.sleep(50);
+            }catch(Exception e)
+            {
+                System.out.println("Can't sleep in display");
+            }
+        }
+        DiningPhilosophers.disp.lock();
+        System.out.println("Philosopher\tState\tTimesEaten");
+        System.out.println("--------------------------------");
+        for(int i = 0; i < DiningPhilosophers.numOfPhilosophers; ++i)
+        {
+            System.out.println(DiningPhilosophers.philArr.get(i).number + "\t\t" + DiningPhilosophers.philArr.get(i).state + "\t\t" + DiningPhilosophers.philArr.get(i).timesEaten);
+        }
+        System.out.println("--------------------------------");
+        DiningPhilosophers.disp.unlock();
     }
 
 }
